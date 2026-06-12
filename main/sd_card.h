b5dddbb03171201549a00212533184cb85e4d2b3
arivas05 <arivas05@proton.me>
2026-06-12 15:43:42 -0500

    pin assignment

--- main/sd_card.h
+++ main/sd_card.h
@@ -43,10 +43,10 @@ pin_configuration_t config = {
 
 // Pin assignments can be set in menuconfig, see "SD SPI Example Configuration" menu.
 // You can also change the pin assignments here by changing the following 4 lines.
-#define PIN_NUM_MISO  CONFIG_EXAMPLE_PIN_MISO
-#define PIN_NUM_MOSI  CONFIG_EXAMPLE_PIN_MOSI
-#define PIN_NUM_CLK   CONFIG_EXAMPLE_PIN_CLK
-#define PIN_NUM_CS    CONFIG_EXAMPLE_PIN_CS
+#define PIN_NUM_MISO  19
+#define PIN_NUM_MOSI  23
+#define PIN_NUM_CLK   18
+#define PIN_NUM_CS    5
 
  esp_err_t s_example_write_file(const char *path, char *data);
 
